72ff3dfa1495d9627c26cd8c2ceef67aa0e78412
Tutudev1223 <122628388+Tutudev1223@users.noreply.github.com>
2023-08-18 16:22:31 -0300

    Add files via upload
    Isso é só um teste

---
+++ testescript.lua
@@ -0,0 +1,30 @@
+while true do 
+function START() 
+end
+--Variaveis
+aimbot = gg.choice("🔥🔫AIMBOT🔫🔥")
+aimlock = gg.choise("🔥🔫AIMLOCK🔫🔥")
+Botão = gg.showUiButton("🔥🥶ENTRE AQUI🥶🔥")
+--condições
+if aimbot == true 
+gg.prompt("🔥🤬AIMBOT TRANQUILO🤬🔥")
+else
+gg.prompt("🔥🤬AIMBOT TÁ RUIM🤬🔥")
+elseif
+gg.prompt("🔥🤬SERÁ QUE ESTÁ RUIM?🤬🔥")
+end
+if aimlock == true 
+gg.prompt("🔥🤬AIMLOCK TRANQUILO🤬🔥")
+else
+gg.prompt("🔥🤬 AIMLOCK TÁ RUIM🤬🔥")
+elseif
+gg.prompt("🔥🤬SERÁ QUE ESTÁ RUIM?🤬🔥")
+end
+if botão == true 
+gg.prompt("🔥🤬BOTÃO TÁ OK🤬🔥")
+else
+gg.gg.prompt("🔥🤬BOTÃO TÁ RUIM🤬🔥")
+elseif
+gg.prompt("🔥🤬 SERÁ QUE ESTÁ RUIM?🤬🔥")
+end
+end
